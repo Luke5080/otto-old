52d27d380f1654d52010d90750be54500bde73b4
Luke5080 <luke360marshall@gmail.com>
2025-02-11 18:20:20 +0000

    Start argument parsing logic

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from otto.ryu.network_state_db.network_state import NetworkState
 from otto.ryu.network_state_db.network_state_updater import NetworkStateUpdater
 from otto.ryu.network_state_db.network_db_operator import NetworkDbOperator
+import argparse
+import toml
 
 def main():
+
+    parser = argparse.ArgumentParser(
+        prog="otto - Intent Based Northbound Interface for SDN Controllers",
+        description="Declare intents to your Software Defined Network!",
+    )
+
+    parser.add_argument("-m", "--model")
+    parser.add_argument("-c", "--controller")
+
+    args = parser.parse_args()
+
+    model = args.model
+
+    controller = args.controller
+
+    """
     ns = NetworkState()
 
     ns.create_network_state_db()
@@ -16,6 +34,7 @@ def main():
 
     except KeyboardInterrupt:
         ndo.drop_database()
+    """
         
 if __name__ == "__main__":
     main()
