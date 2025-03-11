f47123a0f400e0572611796d7686a45d848db4aa
Luke5080 <luke360marshall@gmail.com>
2025-03-11 13:17:16 +0000

    Start create_app OttoShell command logic

--- otto/shell/otto_shell.py
+++ otto/shell/otto_shell.py
@@ -2,13 +2,14 @@ import cmd
 from yaspin import yaspin
 import inquirer
 from langchain_core.messages import HumanMessage
-
+import argparse
 from otto.ryu.network_state_db.network_state import NetworkState
 from otto.ryu.ryu_environment import RyuEnvironment
 from otto.ryu.intent_engine.intent_processor_agent import IntentProcessor
 import sys
 from rich.console import Console
 from rich.markdown import Markdown
+import mysql.connector
 from prettytable import PrettyTable
 
 
@@ -20,6 +21,7 @@ class OttoShell(cmd.Cmd):
     _agent: IntentProcessor
     _verbosity_level: str = "VERBOSE"
     _network_state: NetworkState
+    _create_app_arg_parser: argparse.ArgumentParser
 
     def __init__(self, controller, agent, controller_object):
         super().__init__()
@@ -30,6 +32,12 @@ class OttoShell(cmd.Cmd):
         self._controller_object = controller_object
         self._console = Console()
         self._network_state = NetworkState.get_instance()
+        self._create_app_arg_parser = argparse.ArgumentParser(prog="Create a network application",
+                                                              description="Add app")
+
+        self._database_connection = mysql.connector.connect(
+            user='root', password='root', host='localhost', port=3306, database='network_application_db'
+        )
 
         self.prompt = "otto> "
 
@@ -43,7 +51,7 @@ class OttoShell(cmd.Cmd):
         Agent Output Verbosity Level: {self._verbosity_level}
         """
 
-    def do_get_model(self):
+    def do_get_model(self, line):
         print(self._model)
 
     def do_get_hosts(self, line):
@@ -105,6 +113,23 @@ class OttoShell(cmd.Cmd):
             self.verbose_output(messages)
         else:
             self.non_verbose_output(messages)
+
+    def do_create_app(self, args):
+        self._create_app_arg_parser.add_argument('--name', required=True, help="app name")
+        self._create_app_arg_parser.add_argument('--password', required=True, help="password")
+
+        try:
+            args = self._create_app_arg_parser.parse_args(args.split())
+
+            cursor = self._database_connection.cursor()
+
+            cursor.execute(
+                f"INSERT INTO network_application_db(app_name, password) VALUES({args.app_name}, {args.password})")
+
+            cursor.fetchall()
+
+        except SystemExit:
+            pass
 
     def do_exit(self, arg):
         self._controller_object.stop_state_updater()
