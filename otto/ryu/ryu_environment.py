6ac9ddf27b958ca9abe02014d6192ffac1d85b10
Luke5080 <luke360marshall@gmail.com>
2025-03-25 13:21:03 +0000

    Remove NetworkState and leave topology collection creation logic to done from NetworkDbOperator

--- otto/ryu/ryu_environment.py
+++ otto/ryu/ryu_environment.py
@@ -1,17 +1,20 @@
 import atexit
+
 from otto.controller_environment import ControllerEnvironment
-from otto.ryu.network_state_db.network_state import NetworkState
-from otto.ryu.network_state_db.network_state_finder import NetworkStateFinder
+from otto.ryu.network_state_db.network_db_operator import NetworkDbOperator
 from otto.ryu.network_state_db.network_state_updater import NetworkStateUpdater
 
 
 class RyuEnvironment(ControllerEnvironment):
-    network_state = NetworkState()
-    network_state_updater = NetworkStateUpdater(network_state)
-    network_state_finder = NetworkStateFinder()
+    network_state_updater = NetworkStateUpdater()
+    network_db_operator = NetworkDbOperator()
+
+    def __init__(self):
+        atexit.register(self.drop_database)
+        atexit.register(self.stop_state_updater)
 
     def create_network_state(self):
-        self.network_state.create_network_state_db()
+        self.network_db_operator.create_network_state_db()
 
     def start_state_updater(self):
         self.network_state_updater.start()
@@ -20,4 +23,4 @@ class RyuEnvironment(ControllerEnvironment):
         self.network_state_updater.stop_event.set()
 
     def drop_database(self):
-        self.network_state.network_db_operator.drop_database()
+        self.network_db_operator.drop_database()
