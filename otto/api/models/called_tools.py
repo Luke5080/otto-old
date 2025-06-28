f507951a75ae7130f19603c92e39832d0a996f17
Luke5080 <luke360marshall@gmail.com>
2025-06-29 00:07:54 +0100

    Fixes to schema

--- otto/api/models/called_tools.py
+++ otto/api/models/called_tools.py
@@ -1,12 +1,15 @@
 from otto.api.authentication_db import authentication_db
 
+
 class CalledTools(authentication_db.Model):
     __tablename__ = "called_tools"
 
     id = authentication_db.Column(authentication_db.Integer, nullable=False, primary_key=True)
-    agent_run = authentication_db.Column(authentication_db.String(255), nullable=False)
+    agent_run = authentication_db.Column(authentication_db.String(255),
+                                         authentication_db.ForeignKey('processed_intents.agent_run'), nullable=False)
     run_order = authentication_db.Column(authentication_db.Integer, nullable=False)
-    tool_call_id = authentication_db.Column(authentication_db.String(255), nullable=False)
+    tool_call_id = authentication_db.Column(authentication_db.String(255),
+                                            authentication_db.ForeignKey('tool_calls.id'), nullable=False)
     arguments = authentication_db.Column(authentication_db.Json, nullable=False)
 
     tool_call = authentication_db.relationship('ToolCalls', back_populates='outcomes')
